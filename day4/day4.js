ba4370d7f66889c975fe8d70999e68e3810da42a
Tsiyan <tsia.getachew@gmail.com>
2019-06-06 15:46:57 +0300

    forth day exercise

---
+++ day4/day4.js
@@ -0,0 +1,15 @@
+var book = {
+  titel: "abc",
+  author: "maki",
+  realeasedate: "june 20",}
+  console.log (book.titel)
+   console.log (book.author)
+var food =["enjera","shiro","doro"]
+for (var i=0; i<food.length;i++){
+  console.log (food[i])
+}
+ var number = 1
+ while(number <= 10)
+   {
+     console.log(number++)
+   }
